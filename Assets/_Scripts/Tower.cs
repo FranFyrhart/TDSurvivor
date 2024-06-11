12816ddf33f52a82a5b4fb7286f7fe4d6a0b96d9
FrancisCleo <gfranciscleo@gmail.com>
2024-06-11 14:09:47 +0800

    Tower can now detect enemies

--- Assets/_Scripts/Tower.cs
+++ Assets/_Scripts/Tower.cs
@@ -6,16 +6,23 @@ using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
-    [SerializeField] float attackInterval = 1f;
-    [SerializeField] List<Collider> targetCollidersHit;
-    [SerializeField] LayerMask enemyLayer;
-    [SerializeField] int towerDamage = 100;
-    [SerializeField] Transform connectionPoint; 
+    [Header("Combat settings")]
+    [SerializeField] private float attackRange = 5f;
+    [SerializeField] private int towerDamage = 100;
+    [SerializeField] private float rateOfFire = 1f;
+    [SerializeField] private List<Collider> targetCollidersHit;
+    [SerializeField] private LayerMask enemyLayer;
+    [SerializeField] private Transform connectionPoint;
 
-    Task attackingEnemiesTask;
-    bool _towerDeactivated = true, attackCoroutineRunning = false;
-    Transform _player;
-    IEnumerator attackCoroutine;
+    [Space]
+    [SerializeField] protected Transform projectileSpawnPoint;
+    [SerializeField] protected Transform turretPivot;
+    
+    private bool _towerDeactivated = true, attackCoroutineRunning = false;
+    private Transform _player;
+    private Transform currentTarget;
+    private IEnumerator attackCoroutine;
+    protected List<Transform> targetsInRange = new();
 
     public Transform Player { set { _player = value; } }
 
@@ -39,29 +46,54 @@ public class Tower : MonoBehaviour
 
     private void Update()
     {
-        if (_towerDeactivated)
-            return;
-
         if (_player == null)
             return;
 
         Debug.DrawRay(transform.position, _player.transform.position - transform.position, Color.green);
+
+        if (targetsInRange.Count == 0)
+            return;
+
+        currentTarget = targetsInRange.Aggregate((minItem, nextItem) =>
+            Vector3.Distance(minItem.position, transform.position) < Vector3.Distance(nextItem.position, transform.position) ? minItem : nextItem);
+
+        if (currentTarget == null)
+            return;
+
+        Vector3 targetPosition = currentTarget.transform.position;
+        targetPosition.y = turretPivot.position.y; // Keep the same height as the turretPivot
+        turretPivot.LookAt(targetPosition);
         //Attack();
     }
 
     private void Attack()
     {
-        targetCollidersHit = Physics.OverlapCapsule(connectionPoint.position, _player.position, 1f, enemyLayer).ToList();
+        //targetCollidersHit = Physics.OverlapCapsule(transform.position, _player.position, 1f, enemyLayer).ToList();
+
+        targetCollidersHit = Physics.OverlapSphere(transform.position, attackRange, enemyLayer).ToList();
 
         if (targetCollidersHit.Count == 0) return;
 
         //for each overlap capsule, flash the material of the target
-        for (int j = 0; j < targetCollidersHit.Count; j++)
-        {
-            Enemy enemy = targetCollidersHit[j].GetComponent<Enemy>();
-            if (enemy == null) continue;
-            enemy.TakeDamage(towerDamage);
-        }
+        //for (int j = 0; j < targetCollidersHit.Count; j++)
+        //{
+        //    Enemy enemy = targetCollidersHit[j].GetComponent<Enemy>();
+        //    if (enemy == null) continue;
+        //    enemy.TakeDamage(towerDamage);
+        //}
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Adding target");
+        targetsInRange.Add(other.transform);
+        //if (other.CompareTag(StringConstants.Tags.enemyTag))
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag(StringConstants.Tags.enemyTag))
+            targetsInRange.Remove(other.transform);
     }
 
     public Transform GetConnectionPoint() {
@@ -77,7 +109,7 @@ public class Tower : MonoBehaviour
         while (!_towerDeactivated)
         {
             Debug.Log("Damaging enemies");
-            float currentCooldown = attackInterval;
+            float currentCooldown = 1f / rateOfFire;
             while (currentCooldown > 0)
             {
                 currentCooldown -= 0.1f;
