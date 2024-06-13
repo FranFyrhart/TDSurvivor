3ce2ef2ff14445f5f8b76b1a2b6bef51135668f6
FrancisCleo <gfranciscleo@gmail.com>
2024-06-13 17:17:25 +0800

    Code cleanup

--- Assets/_Scripts/Enemy.cs
+++ Assets/_Scripts/Enemy.cs
@@ -24,10 +24,8 @@ public class Enemy : CombatHandler
 
     private void Awake()
     {
-        //Transform playerTransform = FindObjectOfType<PlayerCharacter>().transform;
         currentHP = baseHP;
         meshAgent = gameObject.GetComponent<NavMeshAgent>();
-        //material = GetComponent<MeshRenderer>().material;
         playerTransform = FindObjectOfType<PlayerMovement>().transform;
     }
 
@@ -40,60 +38,15 @@ public class Enemy : CombatHandler
         if (meshAgent.remainingDistance < attackRange && attackCooldownDone)
         {
             //meshAgent.isStopped = true;
-            StartAttack();
+            StartCoroutine(Attack());
         }
     }
-
-    private void StartAttack() {
-        //if (attackCoroutine != null)
-        //{
-        //    Debug.Log("Starting attack coroutine");
-        //    //attackCoroutine.
-        //    StopCoroutine(attackCoroutine);
-        //    StartCoroutine(attackCoroutine);
-        //    return;
-        //}
-        //attackCoroutine = Attack();
-        StartCoroutine(Attack());
-    }
-
-    //public override void TakeDamage(int damage) {
-    //    Debug.Log("Taking damage");
-    //    currentHP -= damage;
-
-    //    if (currentHP <= 0)
-    //    {
-    //        gameObject.SetActive(false);
-    //        return;
-    //    }
-
-    //    #region visual feedback 
-
-    //    if (colorTween != null)
-    //    {
-    //        if (!colorTween.IsComplete()) {
-                
-    //            Debug.Log("Color tween not yet completed");
-    //            return;
-    //        }
-
-    //        Debug.Log($"playing tween {colorTween.IsActive()}");
-    //        colorTween.Restart();
-    //        return;
-    //    }
-
-    //    colorTween = material.DOColor(Color.black, "_BaseColor", 0.1f).From().SetLoops(1).SetAutoKill(false);
-    //    #endregion
-    //}
-
     private IEnumerator Attack() 
     {
         attackCooldownDone  = false;
         currentCooldown = 1f / attackSpeed;
 
         Projectile newProjectile = Instantiate(projectile, projectileSpawnPoint.position, transform.rotation).GetComponent<Projectile>();
-        //Debug.Log($"Intended spawn position: {transform.position}");
-        // Get the rigidbody component of the projectile
         Rigidbody rb = newProjectile.ProjectileRB;
 
         // Set the velocity of the projectile to the enemy's forward direction multiplied by the speed factor
